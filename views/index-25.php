00bd0b325c5d6985dd44879284fe5ce80f6ed7f5
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-07-30 01:44:29 +0800

    Update the index spcing

--- views/index-25.php
+++ views/index-25.php
@@ -54,11 +54,11 @@
 
     <section class="py-100">
         <div class="container-fluid w-max-1500">
-            
+
         </div>
     </section>
 
-    <section class="pt-100 pb-100" style="background:black;">
+    <section class="pt-600 pb-100" style="background:black;">
         <div class="container-fluid w-max-1500 pb-100">
             <h1 class="text-center text-white">World's Tastiest <br> Flavours</h1>
             <div class="row">
