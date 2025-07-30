3097a2b82ba95ee144eeddd39f0aed0226b66c40
ishakismail7905 <ishakismailwork@gmail.com>
2025-07-30 15:53:12 +0800

    repair-socials-footer

--- Social.php
+++ Social.php
@@ -5,19 +5,19 @@ class Social {
         [
             'wow-delay' => '.35s',
             'url' => 'https://www.instagram.com/mymatecofficial?igsh=MTdzZ3BhcWZtbmF5cA==',
-            'icon' => 'fa-instagram',
+            'icon' => 'fa-brands fa-instagram',
             'target' => '_blank'
         ],
         [
             'wow-delay' => '.2s',
             'url' => 'https://www.facebook.com/people/MATEC-2024/61561885798386/',
-            'icon' => 'fa-facebook',
+            'icon' => 'fa-brands fa-facebook',
             'target' => '_blank'
         ],
         [
             'wow-delay' => '.5s',
             'url' => 'https://www.tiktok.com/@mymatecofficial?_t=ZS-8yRtlw21g9c&_r=1',
-            'icon' => 'fa-tiktok',
+            'icon' => 'fa-brands fa-tiktok',
             'target' => '_blank'
         ],
        
