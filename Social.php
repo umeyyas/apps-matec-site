0a92794581e144bc17a7376e0c278ef954bd5e91
ishakismail7905 <ishakismailwork@gmail.com>
2025-07-30 11:38:08 +0800

    update_socialfooterandbold_nav_2

--- Social.php
+++ Social.php
@@ -4,7 +4,7 @@ class Social {
     private static $socialLinks = [
         [
             'wow-delay' => '.35s',
-            'url' => 'https://www.instagram.com/matecofficial',
+            'url' => 'https://www.instagram.com/mymatecofficial?igsh=MTdzZ3BhcWZtbmF5cA==',
             'icon' => 'fa-instagram',
             'target' => '_blank'
         ],
@@ -16,22 +16,12 @@ class Social {
         ],
         [
             'wow-delay' => '.5s',
-            'url' => 'https://www.tiktok.com/@matec.official?_t=8oFjREcKLMV&_r=1',
+            'url' => 'https://www.tiktok.com/@mymatecofficial?_t=ZS-8yRtlw21g9c&_r=1',
             'icon' => 'fa-tiktok',
             'target' => '_blank'
         ],
-        [
-            'wow-delay' => '.65s',
-            'url' => 'https://www.youtube.com/channel/UCrjPBpBeCsjJuWa-Du9eahQ',
-            'icon' => 'fa-youtube-play',
-            'target' => '_blank'
-        ],
-        [
-            'wow-delay' => '.35s',
-            'url' => 'https://x.com/matecofficial',
-            'icon' => 'fa-x-twitter',
-            'target' => '_blank'
-        ],
+       
+       
     ];
 
     public static function getAll() {
--- views/layouts/navbar-25.php
+++ views/layouts/navbar-25.php
@@ -50,10 +50,10 @@
         <div class="collapse navbar-collapse" id="matecNav">
             <ul class="navbar-nav ml-auto text-center">
                 <li class="nav-item"><a class="nav-link" href="#">HOME</a></li>
-                <li class="nav-item"><a class="nav-link" href="the-ideas">ABOUT US</a></li>
-                <li class="nav-item"><a class="nav-link" href="#matec-carousel-gallery">GALLERY</a></li>
+                <li class="nav-item"><a class="nav-link" href="the-ideas">THE IDEAS</a></li>
+                <li class="nav-item"><a class="nav-link" href="partnership">PARTNERSHIP</a></li>
                 <li class="nav-item"><a class="nav-link" href="faq">F.A.Q</a></li>
-                <li class="nav-item"><a class="nav-link" href="call-us">CALL US</a></li>
+                <li class="nav-item"><a class="nav-link" href="call-us">CONTACT US</a></li>
             </ul>
         </div>
     </nav>
