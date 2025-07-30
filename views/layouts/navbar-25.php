6afeefd2ca1b58354a4fbb2555a0224ec2d5034e
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-07-30 09:53:48 +0800

    Update the navbar countdown

--- views/layouts/navbar-25.php
+++ views/layouts/navbar-25.php
@@ -1,23 +1,23 @@
 <header>
     <div id="count-down-sect" class="container-fluid" style="background-color:#000000;">
         <div class="container">
-            <div class="countdown countdown-25 text-center mb-1">
+            <div class="countdown countdown-25 text-center mb-1 pt-15 pb-1">
                 <div class="w-100 g-1">
-                    <div class="text-center">
-                        <h2 class="">39</h2>
-                        <h4 class="mb-0">DAYS</h4>
+                    <div class="text-center px-10">
+                        <h2 style="font-size:2.25rem;" class="pl-sm-3 pl-2">00</h2>
+                        <h4 style="font-size:1.1rem;" class="mb-0">DAYS</h4>
                     </div>
-                    <div class="text-center">
-                        <h2 class="">39</h2>
-                        <h4 class="mb-0">HOURS</h4>
+                    <div class="text-center px-10">
+                        <h2 style="font-size:2.25rem;" class="pl-sm-3 pl-2">00</h2>
+                        <h4 style="font-size:1.1rem;" class="mb-0">HOURS</h4>
                     </div>
-                    <div class="text-center">
-                        <h2 class="">39</h2>
-                        <h4 class="mb-0">MINUTES</h4>
+                    <div class="text-center px-10">
+                        <h2 style="font-size:2.25rem;" class="pl-sm-3 pl-2">00</h2>
+                        <h4 style="font-size:1.1rem;" class="mb-0">MINUTES</h4>
                     </div>
-                    <div class="text-center">
-                        <h2 class="">39</h2>
-                        <h4 class="mb-0">SECONDS</h4>
+                    <div class="text-center px-10">
+                        <h2 style="font-size:2.25rem;" class="pl-sm-3 pl-2">00</h2>
+                        <h4 style="font-size:1.1rem;" class="mb-0">SECONDS</h4>
                     </div>
                 </div>
             </div>
