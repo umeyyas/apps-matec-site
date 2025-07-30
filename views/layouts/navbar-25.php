0581f838e337916980aa3a1c9eacdacb94fceee3
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-07-30 09:40:44 +0800

    up

--- views/layouts/navbar-25.php
+++ views/layouts/navbar-25.php
@@ -4,19 +4,19 @@
             <div class="countdown text-center mb-1">
                 <div class="w-100 g-1">
                     <div class="text-center">
-                        <h2 class="pl-sm-3 pl-2">39</h2>
+                        <h2 class="">39</h2>
                         <h4 class="mb-0">DAYS</h4>
                     </div>
                     <div class="text-center">
-                        <h2 class="pl-sm-3 pl-2">39</h2>
+                        <h2 class="">39</h2>
                         <h4 class="mb-0">HOURS</h4>
                     </div>
                     <div class="text-center">
-                        <h2 class="pl-sm-3 pl-2">39</h2>
+                        <h2 class="">39</h2>
                         <h4 class="mb-0">MINUTES</h4>
                     </div>
                     <div class="text-center">
-                        <h2 class="pl-sm-3 pl-2">39</h2>
+                        <h2 class="">39</h2>
                         <h4 class="mb-0">SECONDS</h4>
                     </div>
                 </div>
