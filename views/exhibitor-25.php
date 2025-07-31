ad05540a4095c7ffc4a0fd81d090c41ebba13836
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-07-31 10:01:51 +0800

    Update

--- views/exhibitor-25.php
+++ views/exhibitor-25.php
@@ -69,7 +69,8 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-md-12 col-7 order-2 order-sm-1">
-                                <img src="<?= asset('images/2025/partnership_icon.png') ?>" alt="" class="img-fluid newtab" data-pdf-url="<?= asset('pdf/MATEC2024_Event Brief_v7 14082024.pdf') ?>" >
+                                <img src="<?= asset('images/2025/partnership_icon.png') ?>" alt="" class="img-fluid newtab" data-pdf-url="" >
+                                <!-- <?= asset('pdf/MATEC2024_Event Brief_v7 14082024.pdf') ?> -->
                             </div>
                             <div class="col-md-12 col-5 pl-sm-2 pl-0 text-center order-1 order-sm-2 d-flex align-items-center justify-content-center pt-sm-0 pt-10">
                                 <h5 class="mt-3 mb-3" style="color:#ffffff;">MATEC2025 Partnership Packages</h5>
