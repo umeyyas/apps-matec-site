294f155a0a91bd02586f67fbd0d008bdbc7dfede
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-07-31 08:26:57 +0800

    Update

--- views/index-25.php
+++ views/index-25.php
@@ -49,7 +49,7 @@
                     <span class="d-block d-md-inline">STADIUM NASIONAL BUKIT JALIL KUALA LUMPUR</span>
                 </h4>
                 <div class="text-center mt-5 button-container">
-                    <a href="views/visitor.php" class="btn-big-red">Visitor</a>
+                    <a href="views/visitor.php" class="btn-big-red set-to-blue">Visitor</a>
                     <a href="/2025/exhibitor" class="btn-big-red">Exhibitor</a>
                 </div>
             </div>
