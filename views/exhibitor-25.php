96174ff30e7ae9feb78a9ba4b83a3da2b555d568
umeyyas <shihabuddinalfikri01@gmail.com>
2025-08-04 22:54:51 +0800

    edit exhibitor

--- views/exhibitor-25.php
+++ views/exhibitor-25.php
@@ -49,7 +49,7 @@
             Become part of Malaysia’a largest automotive ecosystem fair and enjoy all benefits and programs to help you grow your business. Let’s join and be apart of MATEC2025.
         </h4>
 
-        <div class="row">
+        <div class="row justify-content-center mx-auto">
             <div class="col-md-3 mb-sm-0 mb-60">
                 <div class="card" style="background-image: url(<?= asset('images/2025/bg-exhibitor1820.webp') ?>)">
                     <div class="card-body">
@@ -59,21 +59,6 @@
                             </div>
                             <div class="col-md-12 col-5 pl-sm-2 pl-0 text-center order-1 order-sm-2 py-sm-auto d-flex align-items-center justify-content-center pt-sm-0 pt-10">
                                 <h5 class="mt-3 mb-3" style="color:#ffffff;">MATEC2025 Event Brief</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 mb-sm-0 mb-60">
-                <div class="card" style="background-image: url(<?= asset('images/2025/bg-exhibitor1820.webp') ?>)">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-md-12 col-7 order-2 order-sm-1">
-                                <img src="<?= asset('images/2025/partnership_icon.png') ?>" alt="" class="img-fluid newtab" data-pdf-url="" >
-                                <!-- <?= asset('pdf/MATEC2024_Event Brief_v7 14082024.pdf') ?> -->
-                            </div>
-                            <div class="col-md-12 col-5 pl-sm-2 pl-0 text-center order-1 order-sm-2 d-flex align-items-center justify-content-center pt-sm-0 pt-10">
-                                <h5 class="mt-3 mb-3" style="color:#ffffff;">MATEC2025 Partnership Packages</h5>
                             </div>
                         </div>
                     </div>
@@ -127,7 +112,7 @@
                         </h5>
                     </div>
 
-                    <div id="collapseExhibitorOne" class="collapse show" aria-labelledby="headingExhibitorOne" data-parent="#accordionExhibitor">
+                    <div id="collapseExhibitorOne" class="collapse" aria-labelledby="headingExhibitorOne" data-parent="#accordionExhibitor">
                         <div class="card-body">
                             Over 150 exhibitors with the various of spaces.
                         </div>
