4ca15b914bfba0fecace097a71f22665a32297d5
AL <shihabuddinalfikri01@gmail.com>
2025-08-25 15:34:09 +0800

    change event map

--- views/exhibitor-25.php
+++ views/exhibitor-25.php
@@ -69,7 +69,7 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-md-12 col-7 order-2 order-sm-1">
-                                <img src="<?= asset('images/2025/event_layout_icon.png') ?>" alt="" class="img-fluid newtab" data-pdf-url="<?= asset('images/2025/map_ori_v1_04.png') ?>" >
+                                <img src="<?= asset('images/2025/event_layout_icon.png') ?>" alt="" class="img-fluid newtab" data-pdf-url="<?= asset('images/2025/map_ori_v2_04.png') ?>" >
                             </div>
                             <div class="col-md-12 col-5 pl-sm-2 pl-0 text-center order-1 order-sm-2 d-flex align-items-center justify-content-center pt-sm-0 pt-10">
                                 <h5 class="mt-3 mb-3" style="color:#ffffff;">MATEC2025 Full Layout</h5>
